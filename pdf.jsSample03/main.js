8cd428be107f69ef9b9780c851635928b835257d
hyunsu00 <hyunsu00@hancom.com>
2022-02-22 13:55:44 +0900

    주석 저장 기능 추가

--- pdf.jsSample03/main.js
+++ pdf.jsSample03/main.js
@@ -1,4 +1,7 @@
 // main.js
+let pageHeight = undefined;
+let pdfFactory = undefined;
+
 let PDFJSAnnotate = PDFAnnotate["default"];
 const UI = PDFJSAnnotate.UI;
 let localStoreAdapter = new PDFJSAnnotate.LocalStoreAdapter();
@@ -10,14 +13,9 @@ let MyStoreAdapter = new PDFJSAnnotate.StoreAdapter({
     return localStoreAdapter.getAnnotation(documentId, annotationId);
   },
   addAnnotation(documentId, pageNumber, annotation) {
-    // annotation.x = 312;
-    // annotation.y = 460;
-    // annotation.width = 250;
-    // annotation.height = 170;
-
-    let x_y = [annotation.x, 800 - annotation.y];
-    let coordinates = [x_y[0], x_y[1], x_y[0] + annotation.width, x_y[1] - annotation.height];
-    pdfFactory.createSquareAnnotation(pageNumber - 1, coordinates.slice(), null, null);
+    // let x_y = [annotation.x, pageHeight - annotation.y];
+    // let coordinates = [x_y[0], x_y[1], x_y[0] + annotation.width, x_y[1] - annotation.height];
+    // pdfFactory.createSquareAnnotation(pageNumber - 1, coordinates.slice(), null, null);
     return localStoreAdapter.addAnnotation(documentId, pageNumber, annotation);
   },
   editAnnotation(documentId, pageNumber, annotation) {
@@ -64,6 +62,7 @@ window.onload = () => {
       let pageNumber = 1;
       pdf.getPage(pageNumber).then(function (page) {
         console.log("PDF 파일 페이지 로드됨");
+        pageHeight = page.view[3];
 
         RENDER_OPTIONS.pdfDocument = pdf;
         VIEWER.appendChild(UI.createPage(1));
@@ -93,8 +92,19 @@ window.onload = () => {
     UI.enableRect("area");
   });
 
+  // 다운로드
   document.getElementById("download").addEventListener("click", (e) => {
     console.log("click download");
-    pdfFactory.download();
+
+    let pageNumber = 1;
+    PDFJSAnnotate.getAnnotations(RENDER_OPTIONS.documentId, pageNumber).then(function (annotations) {
+      for (let i = 0; i < annotations.annotations.length; i++) {
+        let annotation = annotations.annotations[i];
+        let x_y = [annotation.x, pageHeight - annotation.y];
+        let coordinates = [x_y[0], x_y[1], x_y[0] + annotation.width, x_y[1] - annotation.height];
+        pdfFactory.createSquareAnnotation(pageNumber - 1, coordinates.slice(), null, null);
+      }
+      pdfFactory.download();
+    });
   });
 };
